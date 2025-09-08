7ec224df6ed52f4565aa044c2d82faa6d2330c93
maicopereira <maicopereira@localhost>
2025-09-08 19:45:00 -0300

    Exercises of Java Language.2

--- src/main/java/com/mycompany/mavenproject1/atividadeEmSala25.java
+++ src/main/java/com/mycompany/mavenproject1/atividadeEmSala25.java
@@ -12,23 +12,23 @@ import javax.swing.*;
  */
 public class atividadeEmSala25 {
     public static void main(String args[]){
-        int i, flag;
+        int i;
+        boolean flag = true;
         int numElementos = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de pessoas a ser cadastrado "));
         String vetorPesquisado[] = new String[numElementos];
         for(i=0; i<numElementos;i++){
             vetorPesquisado[i] = JOptionPane.showInputDialog("Digite o nome para cadastro");
         }
         String elementoProcurado = JOptionPane.showInputDialog("Digite o nome a ser procurado");
-        flag = 0;
         
         for(i=0; i<numElementos;i++){
             if (vetorPesquisado[i].equalsIgnoreCase(elementoProcurado)){
                 JOptionPane.showMessageDialog(null,"o valor procurado foi encontrado na posição "+i);
-            flag = 1;
+            flag = false;
             break;
             }
         }
-        if (flag == 0){
+        if (flag){
             JOptionPane.showMessageDialog(null, "o nome não foi encontrado.");
         }
     }
